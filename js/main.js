5cd1c9f87670abd48a45559328d3a2894f8e4c17
Helixsoft <tushardharaster@gmail.com>
2014-04-05 00:03:21 +0530

    html /css complete

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.toggle-comment').on('click',function(){
+		if($('.comment-area').css('display')=='none'){
+			$('.toggle-comment span').html('HIDE');
+		}if($('.comment-area').css('display')=='block'){
+			$('.toggle-comment span').html('SHOW');
+		}
+		$('.comment-area').slideToggle( "slow" );
+	});
+	var container = document.querySelector('#container');
+	var msnry = new Masonry( container, {
+	  // options
+	  columnWidth: 276,
+	  gutter: 10,
+	  itemSelector: '.item'
+	});
+});
+
+
