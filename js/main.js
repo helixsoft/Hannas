bbb5b400e7508f7ec3b269acb110a971044dab66
tushardhara <tushardharaster@gmail.com>
2014-04-08 23:42:32 +0530

    responsive layout fix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
-	$('.toggle-comment').on('click',function(){
-		if($('.comment-area').css('display')=='none'){
-			$('.toggle-comment span').html('HIDE');
-		}if($('.comment-area').css('display')=='block'){
-			$('.toggle-comment span').html('SHOW');
-		}
-		$('.comment-area').slideToggle( "slow" );
-	});
-	var container = document.querySelector('#container');
-	var msnry = new Masonry( container, {
-	  // options
-	  columnWidth: 265,
-	  gutter: 10,
-	  itemSelector: '.item'
-	});
+	if( !device.tablet() && !device.mobile() ) {
+		$('.toggle-comment').on('click',function(){
+			if($('.comment-area').css('display')=='none'){
+				$('.toggle-comment span').html('HIDE');
+			}if($('.comment-area').css('display')=='block'){
+				$('.toggle-comment span').html('SHOW');
+			}
+			$('.comment-area').slideToggle( "slow" );
+		});
+	}else{
+		$('.toggle-comment span').html('');
+		$('.comment-area').show();
+	}
+	/* only play video on desktop devices */
+	if( !device.tablet() && !device.mobile() ) {
+		var container = document.querySelector('#container');
+		var msnry = new Masonry( container, {
+		  // options
+		  columnWidth: $(document).width() > 800 ? 265 : $(document).width() >= 600 ? 275 : 145 ,
+		  gutter: 10,
+		  itemSelector: '.item'
+		});
+	}
 });
 
 
